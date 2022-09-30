b239d7cf5b74a215cf196b37c0f6874be2e3bf8e
chaitanyatechsophy <chaitanya.k@techsophy.com>
2022-09-30 14:12:11 +0530

    cloud-cluster

--- install-artifacts/setup-keycloak-admin.sh
+++ install-artifacts/setup-keycloak-admin.sh
@@ -1,5 +1,5 @@
-kubectl delete -f install-admin.yaml
-kubectl delete configmap admin-config
+kubectl delete -f install-admin.yaml -n dev
+kubectl delete configmap admin-config -n dev
 
-kubectl create configmap admin-config --from-file=keycloak_clientscopes.postman_collection.json 
-kubectl apply -f install-admin.yaml
+kubectl create configmap admin-config --from-file=keycloak_clientscopes.postman_collection.json  -n dev
+kubectl apply -f install-admin.yaml -n dev
--- install-artifacts/setup-menu.sh
+++ install-artifacts/setup-menu.sh
@@ -1,4 +1,4 @@
-kubectl delete -f install-menu.yaml
-kubectl delete configmap menu-config
-kubectl create configmap menu-config --from-file=menu_artifacts
-kubectl apply -f install-menu.yaml
+kubectl delete -f install-menu.yaml -n dev
+kubectl delete configmap menu-config -n dev
+kubectl create configmap menu-config --from-file=menu_artifacts -n dev
+kubectl apply -f install-menu.yaml -n dev
