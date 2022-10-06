98bfccbf83d01c92b1acab517cf04db2a82ef1a3
nandini <nandini.k@techsophy.com>
2022-10-06 17:49:39 +0530

    comments are done

--- install-artifacts/setup-keycloak-admin.sh
+++ install-artifacts/setup-keycloak-admin.sh
@@ -1,8 +1,3 @@
-#kubectl delete -f install-admin.yaml -n dev
-#kubectl delete configmap admin-config -n dev
-#
-#kubectl create configmap admin-config --from-file=keycloak_clientscopes.postman_collection.json  -n dev
-#kubectl apply -f install-admin.yaml -n dev
 namespace=default
 while getopts n: flag
 do
@@ -10,7 +5,6 @@ do
         n) namespace=${OPTARG};;
     esac
 done
-echo $namespace
 kubectl delete -f install-admin.yaml -n $namespace
 kubectl delete configmap admin-config -n  $namespace
 
--- install-artifacts/setup-menu.sh
+++ install-artifacts/setup-menu.sh
@@ -1,7 +1,3 @@
-#kubectl delete -f install-menu.yaml -n dev
-#kubectl delete configmap menu-config -n dev
-#kubectl create configmap menu-config --from-file=menu_artifacts -n dev
-#kubectl apply -f install-menu.yaml -n dev
 namespace=default
 while getopts n: flag
 do
@@ -9,7 +5,6 @@ do
         n) namespace=${OPTARG};;
     esac
 done
-echo $namespace
 kubectl delete -f install-menu.yaml -n $namespace
 kubectl delete configmap menu-config -n $namespace
 kubectl create configmap menu-config --from-file=menu_artifacts -n $namespace
